b287d1fc3c5a64c41afd8d66a3ccdb1c0ca08157
Hew Jun-Wei <junwei.hew@gmail.com>
2018-05-18 17:48:49 +0800

    Adding utility functions, driver sample

--- LeetCode/2 Add Two Numbers/main.cpp
+++ LeetCode/2 Add Two Numbers/main.cpp
@@ -14,6 +14,8 @@ Example:
 	Explanation : 342 + 465 = 807
 */
 
+#include <iostream>
+
 // Definition for singly-linked list
 struct ListNode {
 	int val;
@@ -22,6 +24,88 @@ struct ListNode {
 };
 
 
+// function prototypes
+ListNode* createNode(int x);
+void deleteNode(ListNode* pNode);
+void deleteList(ListNode* pHead);
+void printList(ListNode* pHead);
+ListNode* addToFront(ListNode* pHead, ListNode* pNode);
+
+// test case(s)
+void testExample();
+
+// function to implement:
+ListNode* addTwoNumbers(ListNode* l1, ListNode* p2) {
+	ListNode* pSum = nullptr;
+
+	// implement addition here
+
+	return pSum;
+}
+
+
 int main() {
+	testExample();
+
 	return 0;
+}
+
+
+ListNode* createNode(int x) {
+	return new ListNode(x);
+}
+
+
+void deleteNode(ListNode* pNode) {
+	delete pNode;
+	pNode = nullptr;
+}
+
+
+void deleteList(ListNode* pHead) {
+	ListNode* pCurr = pHead;
+	while (pCurr != nullptr) {
+		pHead = pHead->next;
+		deleteNode(pCurr);
+		pCurr = pHead;
+	}
+}
+
+
+void printList(ListNode* pHead) {
+	while (pHead != nullptr) {
+		std::cout << pHead->val << " -> ";
+		pHead = pHead->next;
+	}
+
+	std::cout << "nullptr" << std::endl;
+}
+
+
+ListNode* addToFront(ListNode* pHead, ListNode* pNode) {
+	pNode->next = pHead;
+	return pNode;
+}
+
+
+void testExample() {
+	// driver sample
+	ListNode* p1 = createNode(3);
+	p1 = addToFront(p1, createNode(4));
+	p1 = addToFront(p1, createNode(2));
+
+	ListNode* p2 = createNode(4);
+	p2 = addToFront(p2, createNode(6));
+	p2 = addToFront(p2, createNode(5));
+
+	printList(p1);
+	printList(p2);
+
+	ListNode* pSum = addTwoNumbers(p1, p2);
+	printList(pSum);
+
+	// cleanup
+	deleteList(p1);
+	deleteList(p2);
+	deleteList(pSum);
 }
