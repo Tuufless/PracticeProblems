a933657015f87856aa65c9407d6882c527db5e9d
Hew Jun-Wei <junwei.hew@gmail.com>
2018-06-04 17:20:17 +0800

    Adding preliminary solution

--- LeetCode/6 Zigzag Conversion/main.cpp
+++ LeetCode/6 Zigzag Conversion/main.cpp
@@ -31,10 +31,56 @@ P     I
 
 #include <string>
 
+bool testExample();
+
 std::string convert(const std::string& s, int numRows) {
-	return "";
+	std::string result;
+
+	int n = numRows;
+
+	for (size_t r = 0; r < numRows; ++r) {
+		size_t i = r;
+
+		if (i < s.length()) {
+			result.push_back(s.at(i));
+		}
+
+		while (i < s.length()) {
+			int diff = 2 * (n - r - 1);
+			if (diff > 0) {
+				i += diff;
+				if (i < s.length()) {
+					result.push_back(s.at(i));
+				}
+				else {
+					break;
+				}
+			}
+
+			diff = 2 * r;
+			if (diff > 0) {
+				i += diff;
+				if (i < s.length()) {
+					result.push_back(s.at(i));
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
+
+	return result;
 }
 
 int main() {
+	testExample();
 	return 0;
+}
+
+bool testExample() {
+	bool b1 = convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR";
+	bool b2 = convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI";
+
+	return b1 && b2;
 }
